2046e5abf7a206893f4c9cca9d2ad8278e6ab7f6
Dan Itsara <dan@glazziq.com>
2019-05-21 13:14:32 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Rails 5.2 Skeleton
+
+This is a skeleton Rails 5.2 app used as a base for various sample problems.
+
+# How to Run (local)
+
+- Install [Ruby 2.6](https://www.ruby-lang.org/en/)
+- Install [Rails](https://guides.rubyonrails.org/getting_started.html)
+- `bundle install`
+- `rails db:setup`
+- `rails server`
+- Open your browser to `http://localhost:3000`
+
+# How to Run (docker/compose)
+
+- `docker-compose run --rm web bundle install`
+- `docker-compose run --rm web rails db:setup`
+- `docker-compose up`
---
+++ db/seeds.rb
@@ -0,0 +1,202 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Document.create title: "The Rat Story", body: <<~HEREDOC
+    In Shostka in 1885, the Mousekewitzes, a Russian-Jewish family of mice who
+    live with a human family named Moskowitz, are having a celebration of
+    Hanukkah where Papa gives his hat to his 5-year-old son, Fievel, and tells
+    him about the United States, a country where there are no cats. The
+    celebration is interrupted when a battery of Cossacks ride through the
+    village square in an anti-Jewish arson attack and their cats likewise
+    attack the village mice.  Because of this, the Moskowitz home, along with
+    that of the Mousekewitzes, is destroyed.
+
+    In Hamburg, the Mousekewitzes board a tramp steamer headed for New York
+    City.  All the mice aboard are ecstatic at the process of going to America
+    as there are "no cats" there. During a thunderstorm on their journey,
+    Fievel suddenly finds himself separated from his family and washed
+    overboard. Thinking that he has died, they proceed to the city as planned,
+    though they become depressed at his loss.
+
+    However, Fievel floats to New York City in a bottle and, after a pep talk
+    from a French pigeon named Henri, embarks on a quest to find his family. He
+    is waylaid by conman Warren T. Rat, who gains his trust and then sells him
+    to a sweatshop. He escapes with Tony Toponi, a street-smart Italian mouse,
+    and they join up with Bridget, an Irish mouse trying to rouse her fellow
+    mice to fight the cats. When a gang of them called the Mott Street Maulers
+    attacks a mouse marketplace, the immigrant mice learn that the tales of a
+    cat-free country are not true.
+
+    Bridget takes Fievel and Tony to see Honest John, an alcoholic politician
+    who knows the city's voting mice. However, he can't help Fievel search for
+    his family, as they have not yet registered to vote. Meanwhile, his older
+    sister, Tanya, tells her gloomy parents she has a feeling that he is still
+    alive, but they insist that it will eventually go away.
+
+    Led by the rich and powerful Gussie Mausheimer, the mice hold a rally to
+    decide what to do about the cats. Warren is extorting them all for
+    protection that he never provides. No one knows what to do about it, until
+    Fievel whispers a plan to Gussie. Although his family also attends, they
+    stand well in the back of the audience and they are unable to recognize
+    Fievel onstage with her.
+
+    The mice take over an abandoned museum on the Chelsea Piers and begin
+    constructing their plan. On the day of launch, Fievel gets lost and
+    stumbles upon the Maulers' lair. He discovers Warren there and that he is
+    actually a small cat in disguise and the leader of the Maulers. They
+    discover, capture and imprison Fievel, but his guard is a reluctant member
+    of the gang, a goofy, soft-hearted long-haired orange tabby cat named
+    Tiger, who befriends and frees him.
+
+    Fievel races back to the pier with the cats chasing after him. There the
+    disguised Warren attempts to extort the mice again to surrender Fievel and
+    their money in exchange for their safety, but when Tony removes Warren's
+    disguise in front of the mice with the aid of a slingshot, they defiantly
+    refuse. Warren then sets fire to the pier's entrance in an attempt to burn
+    the mice alive, until Gussie finally orders the mice to release the secret
+    weapon.  A huge mechanical mouse, inspired by the bedtime tales Papa told
+    Fievel of the "Giant Mouse of Minsk", chases Warren and his gang down the
+    pier and into the water. A tramp steamer bound for Hong Kong picks them up
+    on its anchor and carries them away. However, a pile of leaking kerosene
+    cans has caused remnants of the fire Warren had started to ignite the
+    entire pier, and the mice are forced to flee when the human fire department
+    arrives to extinguish it.
+
+    During the fire, Fievel is once again separated from his family and ends up
+    at an orphanage. Papa and Tanya overhear Bridget and Tony calling out to
+    Fievel, but Papa is sure that there may be another "Fievel" somewhere,
+    until Mama finds his hat.
+
+    Joined by Gussie, Tiger allows them to ride him in a final effort to find
+    Fievel and they are ultimately successful. The journey ends with Henri
+    taking everyone to see his newly completed project—the Statue of Liberty,
+    which appears to smile and wink at Fievel and Tanya, and the Mouskewitzes'
+    new life in the United States begins.
+  HEREDOC
+
+
+Document.create title: "Survival: Ancient Lizard Genocide", body: <<~HEREDOC
+    During the age of the dinosaurs, a massive famine forces several herds of
+    dinosaurs to seek an oasis known as the Great Valley. Among these, a mother
+    in a diminished "longneck" herd gives birth to a single baby, named
+    Littlefoot. Years later, Littlefoot plays with Cera, a "three-horn", until
+    her father intervenes, whereupon Littlefoot's mother describes the
+    different kinds of dinosaurs: "three-horns", "spiketails", "swimmers", and
+    "flyers". That night, as Littlefoot follows a "hopper", he encounters Cera
+    again, and they play together briefly until a large "Sharptooth" attacks
+    them. Littlefoot's mother comes to their rescue, but receives fatal
+    injuries in the process. An earthquake swallows up the Sharptooth and
+    divides Littlefoot and Cera from their herds. Littlefoot receives advice
+    from his dying mother before she passes away. Confused and in grief,
+    Littlefoot meets an old "clubtail" named Rooter, who consoles him. He is
+    then guided by his mother's voice telling him to follow the sun to the
+    Great Valley.
+
+    Later, Littlefoot meets a "bigmouth" named Ducky and a flyer named Petrie,
+    who accompany him on his journey. Cera, who is attempting to find her own
+    kind, finds the unconscious Sharptooth inside a ravine and mistakenly wakes
+    him up. She escapes and bumps into Littlefoot, Ducky, and Petrie; she tells
+    them that the Sharptooth is alive, but Littlefoot does not believe her. As
+    Cera describes her encounter, she accidentally flings Ducky to the
+    direction of a lone hatching spiketail, whom she names Spike and inducts
+    into the group. Seeking the Great Valley, they discover a cluster of trees,
+    which is abruptly depleted by a herd of Longnecks. Searching for remaining
+    growth, they discover a single leaf-bearing tree, and obtain food by
+    stacking up atop each other and pulling it down. Cera remains aloof, but at
+    nightfall, everyone including herself gravitates to Littlefoot's side for
+    warmth and companionship.
+
+    The next morning, they are attacked by the Sharptooth, but they escape
+    through a tunnel too small for him. Beyond this, they discover landmarks
+    mentioned by Littlefoot's mother. Cera stubbornly decides to go another
+    way, but Littlefoot refuses, and their ensuing fight leads to a schism that
+    divides him from the others. However, when Ducky and Spike become
+    endangered by lava and Petrie gets stuck in a tar pit, he returns to rescue
+    them. They then rescue Cera as she is being ambushed by a pack of
+    "domeheads". Ashamed of her fear and behavior, and reluctant to admit her
+    mistake, Cera leaves them in tears. Later, while crossing a pond, Petrie
+    overhears the Sharptooth nearby. The group devises a scheme to lure him to
+    the pond and drown him in the deep side using a nearby boulder. During the
+    ensuing struggle, a draft from the Sharptooth's nostrils enables Petrie to
+    fly for the first time.
+
+    The plan nearly fails when the Sharptooth begins attacking the boulder
+    while the group attempts to push it onto him. However, Cera reunites with
+    the group, and she allows them to push the Sharptooth and the boulder into
+    the water below. The Sharptooth momentarily takes Petrie down with him, but
+    he later emerges unharmed. Littlefoot, alone, follows a cloud resembling
+    his mother, which guides him to the Great Valley. He is then joined by the
+    others. Upon arrival, the five are reunited with their families: Petrie
+    impresses his family with his newfound flight; Ducky introduces Spike to
+    her family, who adopt him; Cera reunites with her father; and Littlefoot
+    rejoins with his grandparents. The group then rejoins at the top of a hill
+    and embrace each other in a hug.
+  HEREDOC
+
+
+Document.create title: "The Fall of the Great King Scar", body: <<~HEREDOC
+    In the Pride Lands of Africa, a pride of lions rule over the animal kingdom
+    from Pride Rock. King Mufasa's and Queen Sarabi's newborn son, Simba (whose
+    name is Swahili for lion[4]), is presented to the gathering animals by
+    Rafiki the baboon (with the colorful facial marking of a mandrill)[5] who
+    serves as the shaman and advisor. Mufasa shows Simba the Pride Lands and
+    explains to him the responsibilities of kingship and the "circle of life",
+    which connects all living things. Mufasa's younger brother, Scar, covets
+    the throne and plots to eliminate Mufasa and Simba, so he may become king.
+    He tricks Simba and his best friend Nala (to whom Simba is betrothed) into
+    exploring a forbidden elephants' graveyard, where they are attacked by
+    three spotted hyenas, Shenzi, Banzai, and Ed, who are in league with Scar.
+    Mufasa is alerted about the incident by his majordomo, the hornbill Zazu,
+    and rescues the cubs. Though upset with Simba, Mufasa forgives him and
+    explains that the great kings of the past watch over them from the night
+    sky, from which he will one day watch over Simba.
+
+    Scar sets a trap for his brother and nephew, luring Simba into a gorge and
+    having the hyenas drive a large herd of wildebeest into a stampede that
+    will trample him. He informs Mufasa of Simba's peril, knowing that the king
+    will rush to save his son. Mufasa saves Simba but ends up hanging
+    perilously from the gorge's edge. Scar refuses to help Mufasa, instead
+    sending him falling to his death. He then convinces Simba that the tragedy
+    was Simba's own fault and advises him to leave the kingdom and never
+    return. He orders the hyenas to kill the cub, but Simba escapes. Scar tells
+    the pride that both Mufasa and Simba were killed in the stampede and steps
+    forward as the new king, allowing his three hyena minions and the rest of
+    their large pack to live in the Pride Lands.
+
+    Simba collapses in a desert and is rescued by Timon and Pumbaa, a meerkat
+    and warthog, who are fellow outcasts. Simba grows up in the jungle with his
+    two new friends, living a carefree life under the motto "hakuna matata"
+    ("no worries" in Swahili). Now a young adult, Simba rescues Timon and
+    Pumbaa from a hungry lioness, who turns out to be Nala. She and Simba
+    reunite and fall in love, and she urges him to return home, telling him
+    that Pride Lands have become a drought-stricken wasteland under Scar's
+    reign. Feeling guilty over his father's death, Simba refuses and storms
+    off. He then encounters Rafiki, who tells him that Mufasa's spirit lives on
+    in Simba. Simba is visited by the ghost of Mufasa in the night sky, who
+    tells him that he must take his rightful place as king. Realizing that he
+    can no longer run from his past, Simba decides to return to the Pride
+    Lands.
+
+    Aided by his friends, Simba sneaks past the hyenas at Pride Rock and
+    confronts Scar, who had just struck Sarabi. Scar taunts Simba over his role
+    in Mufasa's death and backs him to the edge of the rock, where he reveals
+    to him that he murdered Mufasa. Enraged, Simba pins Scar to the ground and
+    forces him to reveal the truth to the rest of the pride. Timon, Pumbaa,
+    Rafiki, Zazu, and the lionesses fend off the hyenas while Scar, attempting
+    to escape, is cornered by Simba at the top of Pride Rock. Scar begs for
+    mercy and attempts to blame the hyenas for his actions; Simba spares his
+    life, but orders him to leave the Pride Lands forever (just like Scar
+    demanded Simba to do when he was a cub). Scar attacks his nephew, but Simba
+    manages to toss him from the top of the rock. Scar survives the fall, but
+    is attacked and killed by the hyenas, who overheard his attempt to betray
+    them. Afterwards, Simba takes over the kingship as rain begins to fall. He
+    also makes Nala his queen.
+
+    Later, with Pride Rock restored to its usual state, Rafiki presents Simba
+    and Nala's newborn cub to the assembled animals, continuing the circle of
+    life.
+  HEREDOC
